54b769912fabbe3f320f3df7cb5f21e40fdaadb0
Ilya Travkin <il-travkin@yandex.ru>
2024-10-03 11:29:55 +0300

    03.10

--- materials/tests.py
+++ materials/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from materials.models import Lesson, Course
+from users.models import User
+
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email='admin@example.com')
+        self.course = Course.objects.create(title='Python', description='Знакомство с python', owner=self.user)
+        self.lesson = Lesson.objects.create(title='Python #1', description='Знакомство с python', course=self.course)
+        self.client.force_authenticate(user=self.user)
+
+    def test_course_retrieve(self):
+        url = reverse('materials:course-detail', args=(self.course.pk,))
+        response =self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get('title'), self.course.title
+        )
+
+    def test_course_create(self):
+        url = reverse('materials:course-list')
+        data = {
+            'title': 'Python',
+            'description': 'Знакомство с python'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            Course.objects.all().count(), 4
+        )
+
+
